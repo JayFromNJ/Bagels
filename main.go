f22f79185d9f93fd26abb85c78d5857e6052337d
Jason Wood <jayfromnj@gmail.com>
2021-10-31 18:18:37 -0400

    Create geClue function

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sort"
+	"strings"
 	"time"
 )
 
@@ -15,8 +17,43 @@ func getSecretNumber() string {
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(numbers), func(i, j int) { numbers[i], numbers[j] = numbers[j], numbers[i] })
 
-	return numbers[0] + numbers[1] + numbers[2]
+	toReturn := ""
+	for i := 0; i < NUM_DIGITS; i++ {
+		toReturn = toReturn + numbers[i]
+	}
 
+	return toReturn
+}
+
+func getClue(guess, secretNumber string) string {
+	if guess == secretNumber {
+		return "You got it!"
+	}
+
+	var clues []string
+	guessSplit := strings.Split(guess, "")
+	secretSplit := strings.Split(secretNumber, "")
+
+	for i := 0; i < NUM_DIGITS; i++ {
+		if guessSplit[i] == secretSplit[i] {
+			clues = append(clues, "Fermi")
+		} else if strings.Contains(secretNumber, guessSplit[i]) {
+			clues = append(clues, "Pico")
+		}
+	}
+
+	if len(clues) == 0 {
+		return "Bagels"
+	}
+
+	sort.Strings(clues)
+
+	toReturn := ""
+	for i := 0; i < len(clues); i++ {
+		toReturn = toReturn + clues[i] + " "
+	}
+
+	return toReturn
 }
 
 func main() {
